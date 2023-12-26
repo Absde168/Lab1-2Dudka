8040641ff0844977505c21da6f38dea62b8556c2
B3dRandoms <bdrandoms0@gmail.com>
2023-12-26 13:48:31 +0200

    Initial commit

---
+++ Lab1-2Dudka/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+double f = 1 / Math.Tan(y / x);
+double r = Math.Sqrt(x*x+y*y);
+Console.WriteLine($"r = {r:F2}");
+Console.WriteLine($"f = {f:F2}");
